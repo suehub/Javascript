03eddfc7a4e7a8113a4540b06d6edf6fa4da8758
sue <dldustn2040@gmail.com>
2022-11-10 20:33:58 +0900

    Event

--- main.js
+++ main.js
@@ -10,19 +10,15 @@ const title = document.querySelector("#title");
 const list = document.querySelector(".list"); 
 const items = document.querySelectorAll(".item");
 const btn = document.querySelector("#btn");
+const naver = document.querySelector(".naver")
 
 
+btn.addEventListener("click", function(){
+    alert("탕수육")
+}) 
 
-title.style.color = "red"
-title.style.backgroundColor = "blue" 
-//title.innerHTML = "<input type='text'/ >"   // innnerHTML은 title 돔 안에 html 삽입시키는 것
-title.innerText = "text"   // 문자가 들어감
-btn.innerText = "button"
-
-// list.remove()
-list.firstElementChild.remove()
-list.lastElementChild.innerHTML = "<a>link</a>"
-
-console.log(title, list, items) 
-
-
+naver.addEventListener("click", (e)=>{
+    e.preventDefault(); // 이벤트가 작동하지 않게 함
+    console.log(e)
+    alert("네이버!!!")
+})
