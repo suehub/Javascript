8a5c8e0f33443a9c84eef6da3b7be67ad0a795b6
sue <dldustn2040@gmail.com>
2022-11-10 19:41:08 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// DOM : Document Object Model
+
+
+// const title = document.getElementById("title"); 
+// const list = document.getElementsByClassName("list")
+
+
+// querySelector : 쉽게 id와 class 상관없이 불러올 수 있음
+const title = document.querySelector("#title");
+const list = document.querySelector(".list"); 
+const items = document.querySelectorAll(".item");
+const btn = document.querySelector("#btn");
+
+
+
+title.style.color = "red"
+title.style.backgroundColor = "green" 
+// console.log({   title }) 
+btn.innerText = "button"
+console.log(title, list, items) 
